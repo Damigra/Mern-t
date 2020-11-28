64d2b8b194c62fb37b83d06621bca07d417ca7ff
Your Name <you@example.com>
2020-11-28 19:02:21 +0200

    work with requests

--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -1,6 +1,25 @@
 import React from 'react'
+import { useHttp } from './../hooks/http.hook';
+
 
 export default function AuthPage() {
+    const { loading, request } = useHttp()
+
+
+    const [form, setForm] = React.useState({
+        email: '', password: ''
+    })
+    const changeHandler = event => {
+        setForm({ ...form, [event.target.name]: event.target.value })
+    }
+
+    const registerHandler = async () => {
+        try {
+            const data = await request('/api/auth/register', 'Post', { ...form })
+            console.log('Data', data)
+        } catch (e) { }
+    }
+
     return (
         <div className="row">
             <div className="col s6 offset-s3">
@@ -9,15 +28,16 @@ export default function AuthPage() {
                     <div className="card-content white-text">
                         <span className="card-title">Авторизация</span>
                     </div>
-                    <div   style={{ margin: 30 }} >
+                    <div style={{ margin: 30 }} >
                         <div className="input-field">
                             <input
                                 placeholder="Введите email"
                                 id="email"
                                 type="text"
                                 className="yellow-input"
+                                onChange={changeHandler}
                             />
-                            <label for="email">Email</label>
+                            <label forHtml="email">Email</label>
                         </div>
                         <div className="input-field">
                             <input
@@ -25,13 +45,24 @@ export default function AuthPage() {
                                 id="password"
                                 type="password"
                                 className="yellow-input"
+                                onChange={changeHandler}
                             />
-                            <label for="password">Пароль</label>
+                            <label forHtml="password">Пароль</label>
                         </div>
                     </div>
                     <div className="card-action">
-                        <button className="btn yellow darken-4" style={{ marginRight: 20 }}>Войти</button>
-                        <button className="btn grey lighten-1 black-text">Регестрицая</button>
+                        <button
+                            className="btn yellow darken-4"
+                            style={{ marginRight: 20 }}
+                            disabled={loading}>
+                            Войти
+                             </button>
+                        <button
+                            className="btn grey lighten-1 black-text"
+                            onClick={registerHandler}
+                            disabled={loading}>
+                            Регестрицая
+                             </button>
                     </div>
                 </div>
             </div>
