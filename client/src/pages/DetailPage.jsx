bf4fbd23e9afe0f69d6cb4c1ee5361aba05423b3
Your Name <you@example.com>
2020-11-29 17:55:32 +0200

    Loader

--- client/src/pages/DetailPage.jsx
+++ client/src/pages/DetailPage.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function DetailPage() {
+const [link, setLink] = useState(null)
+
+
     return (
         <div>
 DetailPage
